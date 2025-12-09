67c832ae69b85c777a0f989436d39a2fe40b5dc9
Aidan Mc Intosh <aidanamcintosh@gmail.com>
2025-12-09 11:23:58 -0400

    step 1 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,52 @@
 import { useState } from "react";
 
+const Display = ({ text }) => {
+	return <h1>{text}</h1>;
+};
+
+const Button = ({ text, onClick }) => {
+	return <button onClick={onClick}>{text}</button>;
+};
+
+const Stats = ({ text, figure }) => {
+	return (
+		<p>
+			{text} {figure}
+		</p>
+	);
+};
+
 const App = () => {
 	// save clicks of each button to its own state
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
-	return <div>code here</div>;
+	const handleGoodClick = () => {
+		setGood(good + 1);
+	};
+
+	const handleNeutralClick = () => {
+		setNeutral(neutral + 1);
+	};
+
+	const handleBadClick = () => {
+		setBad(bad + 1);
+	};
+
+	return (
+		<div>
+			<Display text="give feedback" />
+			<Button onClick={handleGoodClick} text="good" />
+			<Button onClick={handleNeutralClick} text="neutral" />
+			<Button onClick={handleBadClick} text="bad" />
+			<Display text="statistics" />
+
+			<Stats text="good" figure={good} />
+			<Stats text="neutral" figure={neutral} />
+			<Stats text="bad" figure={bad} />
+		</div>
+	);
 };
 
 export default App;
